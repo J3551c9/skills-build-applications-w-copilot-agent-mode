05e6e5e9ab882e72c24b91152161b20d186f8d9c
J3551c9 <J3551c9@users.noreply.github.com>
2025-05-07 21:01:26 +0000

    Update settings and views for development environment
    - Modify ALLOWED_HOSTS in settings.py to include localhost and codespace URL.
    - Update api_root function in views.py to use codespace URL for API endpoints.

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-8&g*+s7n!)kdcnwfuiwn$-*bdp7m&2azjj%$#x)m911sua^(%q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', 'humble-pancake-v6xvjw65r5whx46j-8000.app.github.dev']
 
 
 # Application definition
